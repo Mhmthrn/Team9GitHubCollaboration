4c85cb64b6e29a44ca5215f694ce9725356888ac
alimatt1 <ali.matt01@hotmail.com>
2023-03-04 22:49:36 -0500

    ali branch 2.degisiklik

--- src/test/java/tests/Ozellik1.java
+++ src/test/java/tests/Ozellik1.java
@@ -3,5 +3,7 @@ package tests;
 public class Ozellik1 {
     public static void main(String[] args) {
         System.out.println("biraz daha bozmak istedim");
+
+        System.out.println("ali brancinde ikinci degisiklik kendime ozel classda ");
     }
 }
---
+++ src/test/java/tests/Ozellik2.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class Ozellik2 {
+    public static void main(String[] args) {
+        System.out.println("ha ha ha ali brancinde ortak class da degisiklik yaptin");
+
+    }
+}
